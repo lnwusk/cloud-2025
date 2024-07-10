8806358fb43da23eac8e1d6075cae700adec910d
wcx0206 <457529466@qq.com>
2024-07-10 13:48:16 +0800

    create the springboot project

---
+++ FinalProject/src/test/java/com/cloudNative/_4/FinalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudNative._4.FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
